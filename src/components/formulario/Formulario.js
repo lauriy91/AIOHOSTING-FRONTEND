7541add2eeff1cde40b993e60fe75fede25146cc
Sean Daza <60199238+seandaza@users.noreply.github.com>
2021-11-22 09:36:07 -0500

    "Update"

--- src/components/formulario/Formulario.js
+++ src/components/formulario/Formulario.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Form from 'react-bootstrap/Form'
-import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button'
-import FormControl from 'react-bootstrap/FormControl'
+import Card from 'react-bootstrap/Card'
+
 
 
 
 const hStyle = { color: 'purple' };
-
+const btnStyle1 = { color: 'blue',fontWeight: 'bold' };
+const btnStyle2 = { color: 'red',fontWeight: 'bold' };
+const txtBoxStyle = { borderRadius: '40px' };
 
 const Formulario = () => {
     return (
@@ -22,19 +24,19 @@ const Formulario = () => {
                         <Form.Label htmlFor="inlineFormInputName">
                             Nombre Hotel:
                         </Form.Label>
-                        <Form.Control id="inlineFormInputName" placeholder="Hotel Wiwa" />
+                        <Form.Control style={txtBoxStyle} id="inlineFormInputName" placeholder="Hotel Wiwa" />
                     </Col>
                     <Col sm={3} className="my-1">
                         <Form.Label htmlFor="inlineFormInputtype">
                             Tipo de Alojamiento:
                         </Form.Label>
-                        <Form.Control id="inlineFormInputtype" placeholder="Coworking/Coliving" />
+                        <Form.Control style={txtBoxStyle} id="inlineFormInputtype" placeholder="Coworking/Coliving" />
                     </Col>
                     <Col xs="auto" className="my-1">
                         <Form.Label htmlFor="inlineFormInputPrice">
                             Precio:
                         </Form.Label>
-                        <Form.Control id="inlineFormInputPrice" placeholder="$250.000" />
+                        <Form.Control style={txtBoxStyle} id="inlineFormInputPrice" placeholder="$250.000" />
                     </Col>
                 </Row>
                 <Row className="align-items-center">
@@ -42,13 +44,13 @@ const Formulario = () => {
                         <Form.Label htmlFor="inlineFormInputCity">
                             Ciudad:
                         </Form.Label>
-                        <Form.Control id="inlineFormInputCity" placeholder="Medellin" />
+                        <Form.Control style={txtBoxStyle} id="inlineFormInputCity" placeholder="Medellin" />
                     </Col>
                     <Col sm={3} className="my-1">
                         <Form.Label htmlFor="inlineFormInputEscenary">
                             Escenario:
                         </Form.Label>
-                        <Form.Control id="inlineFormInputEscenary" placeholder="medellin" />
+                        <Form.Control style={txtBoxStyle} id="inlineFormInputEscenary" placeholder="medellin" />
                     </Col>
                 </Row>
                 <Row className="align-items-center">
@@ -56,34 +58,70 @@ const Formulario = () => {
                         <Form.Label htmlFor="inlineFormInputTime">
                             Tiempo de Estadia:
                         </Form.Label>
-                        <Form.Control id="inlineFormInputTime" placeholder="15 dias" />
+                        <Form.Control style={txtBoxStyle} id="inlineFormInputTime" placeholder="15 dias" />
                     </Col>
                     <Col sm={3} className="my-1">
                         <Form.Label htmlFor="inlineFormInputPunctuation">
                             Puntuacion:
                         </Form.Label>
-                        <Form.Control id="inlineFormInputPunctuation" placeholder="4.6" />
+                        <Form.Control style={txtBoxStyle} id="inlineFormInputPunctuation" placeholder="4.6" />
                     </Col>
                 </Row>
+                <Row className="align-items-right">
+                    <Col sm={3} className="my-1">
+                    </Col>
+                    <Col sm={3} className="my-1">
+                    </Col>
+                    <Col sm={3} className="my-1">
+                        <Form.Label htmlFor="inlineFormInputPunctuation">
+                            <Button style={btnStyle1} variant="offline-primary">Guardar</Button>
+                            <Button style={btnStyle2} variant="offline-danger">Cancelar</Button>
+                        </Form.Label>
+                    </Col>
+                </Row>
+                <br>
+                </br>
                 <h2 style={hStyle} align="center" >Reservar</h2>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                <div align="right">
+                <Card align="right" style={{ width: '20rem' }}>
+                    <Card.Body>
+                <h5 align="center" style={hStyle}><Form.Label>Ingresa tus Datos</Form.Label></h5>
+                <Form.Group className="mb-3" controlId="formBasicName">
+
+                    <Form.Control style={txtBoxStyle} type="name" placeholder="Nombre" />
+
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                    <Form.Control style={txtBoxStyle} type="email" placeholder="Email" />
+
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+
+                <Form.Group className="mb-3" controlId="formBasicPhone">
+
+                    <Form.Control style={txtBoxStyle} type="telephone" placeholder="Teléfono" />
+
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+
+                <Button style={txtBoxStyle} variant="primary" type="submit">
+                    Comprar
+
                 </Button>
+
+                <div>
+                    <Form.Text className="text-muted">
+                        Política de tratamiento de datos
+                    </Form.Text>
+                    <Form.Check
+                        type="checkbox"
+                        id="autoSizingCheck"
+                        className="mb-2"
+                    />
+                </div>
+                </Card.Body>
+                </Card>
+                </div>
             </Form>
         </>
     );
