dd40f5732b72bae4c0392fab8e3e95b08f341cb9
@petrizzo626 <petrizzo626@gmail.com>
2021-11-26 21:39:31 -0500

    Formulario Reserva

---
+++ src/components/iconos/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+//Iconos
+import { BsBicycle } from "react-icons/bs";
+import { BiCoffeeTogo } from "react-icons/bi";
+import { FiWifi } from "react-icons/fi";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BsBicycle/>
+      <BiCoffeeTogo/>
+      <FiWifi/>
+  
+    </div>
+  );
+}
+
+export default App;
