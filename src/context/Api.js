bcf5063772dd2fb5705fe5acd0f01cfea1a70092
@petrizzo626 <petrizzo626@gmail.com>
2021-11-29 00:56:38 -0500

    ApiContext :globe_with_meridians:

--- src/context/Api.js
+++ src/context/Api.js
@@ -1,6 +1,14 @@
 const server = "http://localhost:3000";
-const apiRegister = `${server}/LoginUser`;
-const apiLoginUser = `${apiRegister}/auth`;
-// const apiRegister = `${apiRegister}/auth`;
+const apiAdmin = `${server}/admin`;
+const apiLogin = `${apiAdmin}/login`
+const apiAlojamiento = `${server}/alojamientos`;
+const apiFiltroNombre = `${server}/nombre`
+const apiFiltroHabitacion = `${apiAlojamiento}/habitacion`
+const apiFiltroUbicacion = `${apiAlojamiento}/ubicacion`
+const apiFiltroPresupuesto = `${apiAlojamiento}/presupuesto`
+const apiFiltroFchaIngreso = `${apiAlojamiento}/fch_ingreso`
+const apiFiltroTEstadia = `${apiAlojamiento}/tmp_estadia`
+const apiUsuario = `${server}/usuarios`
+const apiReserva = `${server}/reservas`
 
-export {apiLoginUser, apiRegister};
+export {apiAdmin, apiLogin, apiAlojamiento, apiUsuario, apiReserva, apiFiltroNombre, apiFiltroHabitacion, apiFiltroUbicacion, apiFiltroPresupuesto, apiFiltroFchaIngreso, apiFiltroTEstadia};
