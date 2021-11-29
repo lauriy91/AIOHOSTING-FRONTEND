7e5675a12ec94c33dc4d94e2139fa85f2048dc0c
Sean Daza <60199238+seandaza@users.noreply.github.com>
2021-11-29 08:27:58 -0500

    "Update"

--- src/components/hosting-map/src/components/btn_nav_inf/Idioma.js
+++ src/components/hosting-map/src/components/btn_nav_inf/Idioma.js
@@ -3,7 +3,9 @@ export const Idioma = () => {
     return (
         <div>
             <button className="idioma">Epañol</button>
+            <span id="separador">|</span>
             <button className="idioma">Moneda</button>
+            <span id="separador">|</span>
             <button className="idioma">Login</button>
         </div>
     )
