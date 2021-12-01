d417a8873974c0c34e90154ed647a6945803fb2b
lauriy91 <lauriys91@gmail.com>
2021-11-30 23:57:42 -0500

    incorporating chatbot

--- src/components/vistas/Home.js
+++ src/components/vistas/Home.js
@@ -2,12 +2,18 @@
 // import Paginacion from "../paginacion/Paginacion"
 // import { Cards } from "./Cards"
 // import { Banner } from "../banner/Banner"
-import { TableCardHorizontal } from "../tableCardHorizontal/TableCardHorizontal"
+// import { TableCardHorizontal } from "../tableCardHorizontal/TableCardHorizontal"
+// import { Footer } from "../footer/Footer"
+import { Chatbot } from "../chatbot/Chatbot"
 
 export const Home = () => {
     return (
-        // <Cards/>
-        // <Banner/>
-        <TableCardHorizontal/>
+        // <Banner2/>,
+        // <Cards/>,
+        // <Banner/>,
+        // <TableCardHorizontal/>,
+        // <Paginacion/>
+        // <Footer/>
+        <Chatbot/>
     )
 }
