8b6edf11928ca2bf2490335688eaad475abd5b24
dromero1994 <jromeroparr@gmail.com>
2021-12-01 22:49:41 -0500

    Update style login

--- src/components/loginUser/LoginUser.js
+++ src/components/loginUser/LoginUser.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router";
 import AuthContext from "../../context/AuthContext";
-import {Form, Button} from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import loginUser from "../loginUser/loginUser.css";
 
 const objForm = {
@@ -11,53 +11,58 @@ const objForm = {
 
 const LoginUser = () => {
   //Contexto
-  const {handleLogin} = useContext(AuthContext);
+  const { handleLogin } = useContext(AuthContext);
   //Estados
   const [form, setForm] = useState(objForm);
   //Navegador
   const navigate = useNavigate();
 
-  const handleForm = (e)=>{
-    setForm( {...form, [e.target.name]: e.target.value} );
+  const handleForm = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   }
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    handleLogin(form).then(async (resp)=>{
-      if(resp.status === 200){
-        console.log('Ingreso...');
+    handleLogin(form).then(async (resp) => {
+      if (resp.status === 200) {
         let json = await resp.json();
         let token = json.token;
         localStorage.setItem('token', token);
         navigate('/');
-      }else{
-        alert('Invalid credentials');
+      } else {
+        alert('Credenciales invalidas');
       }
-    }).catch(error=>{
+    }).catch(error => {
       console.log(error);
     });
   }
 
-    return (
-      <div className="login">
-      <h2>Login</h2>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="loginEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control required value={form.correo} onChange={handleForm} name="correo" type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="loginPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control required value={form.contrasena} onChange={handleForm} name="contrasena" type="password" placeholder="Password" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Enter
-        </Button>
-      </Form>
-    </div>
-    );
+  return (
+    <body className="bodyLogin">
+        <Form onSubmit={handleSubmit} className="form">
+          <h2 className="form_title">Iniciar Sesión</h2>
+          <div className="form_container">
+            <div className="form_group">
+              <Form.Group className="mb-3" controlId="loginEmail">
+                <Form.Label>Correo:</Form.Label>
+                <Form.Control required value={form.correo} onChange={handleForm} name="correo" className="form_control" type="email" placeholder="Enter email" />
+                <span className="form_line"></span>
+              </Form.Group>
+            </div>
+            <div className="form_group">
+            <Form.Group className="mb-3" controlId="loginPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control required value={form.contrasena} onChange={handleForm} name="contrasena" className="form_control" type="password" placeholder="Password" />
+              <span className="form_line"></span>
+            </Form.Group>
+            </div>
+            <Button variant="primary" type="submit" className="form_submit">
+              Ingresar
+            </Button>
+          </div>
+        </Form>
+      </body>
+  );
 };
 
 export default LoginUser;
